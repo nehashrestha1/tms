eb9cfd53daba6511ce48e5f49e1f249fe39b631e
nehashrestha1 <142712634+nehashrestha1@users.noreply.github.com>
2024-06-30 12:17:59 +0545

    Feat:code added in login

--- auth/login.php
+++ auth/login.php
@@ -1,39 +1,47 @@
 <?php
 
-require ('../config/config.php');
+require('../config/config.php');
 
 session_start();
 
 if (isset($_POST['login'])) {
-    // $username= $_POST['username'];
     $email = $_POST['email'];
-    $password = password_hash($_POST['password'], PASSWORD_DEFAULT);
-    
+    $password = $_POST['password'];
+
     if ($email != "" && $password != "") {
+        // Use prepared statements to prevent SQL injection
+        $query = "SELECT * FROM users WHERE email = ?";
+        $stmt = mysqli_prepare($conn, $query);
         
-        $query = "SELECT *FROM users WHERE email='$email' AND password='$password'";
-        $result = mysqli_query($conn, $query);
-        
-        $data = mysqli_num_rows($result); // read individual row 
+        if ($stmt) {
+            mysqli_stmt_bind_param($stmt, "s", $email);
+            mysqli_stmt_execute($stmt);
+            $result = mysqli_stmt_get_result($stmt);
 
-        if ($data == 0) {
-            $row = mysqli_fetch_assoc($result);
-            
-            $_SESSION['id'] = $row['id'];
-            $_SESSION['name'] = $row['name'];
-            $_SESSION['username'] = $row['username'];
-            $_SESSION['email'] = $row['email'];
+            if ($result && mysqli_num_rows($result) > 0) {
+                $row = mysqli_fetch_assoc($result);
+                // Verify the password
+                if (password_verify($password, $row['password'])) {
+                    // Set session variables
+                    $_SESSION['id'] = $row['id'];
+                    $_SESSION['name'] = $row['name'];
+                    $_SESSION['username'] = $row['username'];
+                    $_SESSION['email'] = $row['email'];
 
-            echo "<meta http-equiv=\"refresh\" content=\"0;URL=../dashboard.php?msg=success\">";
-
+                    echo "<meta http-equiv=\"refresh\" content=\"0;URL=../dashboard.php?msg=success\">";
+                } else {
+                    echo "<meta http-equiv=\"refresh\" content=\"0;URL=../index.php?msg=error\">";
+                }
+            } else {
+                echo "<meta http-equiv=\"refresh\" content=\"0;URL=../index.php?msg=error\">";
+            }
+            mysqli_stmt_close($stmt);
         } else {
             echo "<meta http-equiv=\"refresh\" content=\"0;URL=../index.php?msg=error\">";
-
         }
     } else {
         echo "<meta http-equiv=\"refresh\" content=\"0;URL=../index.php?msg=warning\">";
     }
 }
-
 
 ?>
