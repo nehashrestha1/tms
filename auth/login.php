de1459c1b205d4171160260cdf79d6fcc1631e64
nehashrestha1 <142712634+nehashrestha1@users.noreply.github.com>
2024-07-01 11:44:10 +0545

    Feat: change in login

--- auth/login.php
+++ auth/login.php
@@ -1,30 +1,37 @@
-<?php 
+<?php
 require ('../config/config.php');
-session_start();
-if (isset($_POST["login"])){
-    $email = $_POST["email"];
-    $password = password_hash($_POST["password"],PASSWORD_DEFAULT);
 
-    if($email != "" && $password !== ""){
-        $sql = "SELECT *FROM users WHERE email = '$email' AND password = '$password'";
-        $result = mysqli_query($conn, $sql);
+if (isset($_POST['login'])) {
+    $email = $_POST['email'];
+    $password = $_POST['password'];
 
+    if ($email != "" && $password != "") {
+        $query = "SELECT * FROM users WHERE email='$email' ";
+        $result = mysqli_query($conn, $query);
+        // var_dump($result);
+        // $count = mysqli_num_rows($result);
+        // var_dump($count);
 
+        if ($result->num_rows === 1) {
+            $user = $result->fetch_assoc();
+            // var_dump($user);
+            if (password_verify($password, $user["password"])) {
+                session_start();
+                $_SESSION['id'] = $user['id'];
+                $_SESSION['username'] = $user['username'];
+                $_SESSION['email'] = $user['email'];
 
-        if(mysqli_num_rows($result)>0){
-            $row = mysqli_fetch_assoc($result);
-            
-            $_SESSION["id"] = $row["id"];
-            $_SESSION["name"] = $row["name"];
-            $_SESSION["email"] = $row["email"];
+                echo header("Location: ../dashboard.php?msg=login_success");
+            } else {
+                echo header("Location: ../index.php?msg=password_error");
+            }
 
-            echo header("<meta http-equiv=\"refresh\" content=\"2;URL=../index.php\">");
+        } else {
+            echo header("Location: ../index.php?msg=login_failed");
         }
-        else{
-             
-        }
-    } else{
-        echo" Email and Password is required" ;
+    } else {
+        echo "All fields are necessary.";
+        header('Refresh: 1; url=../index.php');
     }
 }
 ?>
