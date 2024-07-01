2c6616c6e87ba23bb61254981ef5c6c4b5798390
nehashrestha1 <142712634+nehashrestha1@users.noreply.github.com>
2024-07-01 11:02:42 +0545

    Feat:Changed the name of folder

--- auth/login.php
+++ auth/login.php
@@ -1,47 +1,30 @@
-<?php
-
-require('../config/config.php');
-
+<?php 
+require ('../config/config.php');
 session_start();
+if (isset($_POST["login"])){
+    $email = $_POST["email"];
+    $password = password_hash($_POST["password"],PASSWORD_DEFAULT);
 
-if (isset($_POST['login'])) {
-    $email = $_POST['email'];
-    $password = $_POST['password'];
+    if($email != "" && $password !== ""){
+        $sql = "SELECT *FROM users WHERE email = '$email' AND password = '$password'";
+        $result = mysqli_query($conn, $sql);
 
-    if ($email != "" && $password != "") {
-        // Use prepared statements to prevent SQL injection
-        $query = "SELECT * FROM users WHERE email = ?";
-        $stmt = mysqli_prepare($conn, $query);
-        
-        if ($stmt) {
-            mysqli_stmt_bind_param($stmt, "s", $email);
-            mysqli_stmt_execute($stmt);
-            $result = mysqli_stmt_get_result($stmt);
 
-            if ($result && mysqli_num_rows($result) > 0) {
-                $row = mysqli_fetch_assoc($result);
-                // Verify the password
-                if (password_verify($password, $row['password'])) {
-                    // Set session variables
-                    $_SESSION['id'] = $row['id'];
-                    $_SESSION['name'] = $row['name'];
-                    $_SESSION['username'] = $row['username'];
-                    $_SESSION['email'] = $row['email'];
 
-                    echo "<meta http-equiv=\"refresh\" content=\"0;URL=../dashboard.php?msg=success\">";
-                } else {
-                    echo "<meta http-equiv=\"refresh\" content=\"0;URL=../index.php?msg=error\">";
-                }
-            } else {
-                echo "<meta http-equiv=\"refresh\" content=\"0;URL=../index.php?msg=error\">";
-            }
-            mysqli_stmt_close($stmt);
-        } else {
-            echo "<meta http-equiv=\"refresh\" content=\"0;URL=../index.php?msg=error\">";
+        if(mysqli_num_rows($result)>0){
+            $row = mysqli_fetch_assoc($result);
+            
+            $_SESSION["id"] = $row["id"];
+            $_SESSION["name"] = $row["name"];
+            $_SESSION["email"] = $row["email"];
+
+            echo"<meta http-equiv=\"refresh\" content=\"2;URL=index.php\">";
         }
-    } else {
-        echo "<meta http-equiv=\"refresh\" content=\"0;URL=../index.php?msg=warning\">";
+        else{
+             
+        }
+    } else{
+        echo" Email and Password is required" ;
     }
 }
-
 ?>
